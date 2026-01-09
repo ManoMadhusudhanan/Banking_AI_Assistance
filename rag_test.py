21394da1f16d128e570fa47cc0d3fccd40ddb2c2
Mano MadhuSudhanan <manomadhusudhanan@gmail.com>
2026-01-09 11:27:38 +0530

    Update rag_test.py

--- rag_test.py
+++ rag_test.py
@@ -2,9 +2,10 @@ from qdrant_client import QdrantClient
 import os
 
 client = QdrantClient(
-    url="https://c299fd03-1832-4dc4-8e2e-af6b0fd684f7.us-east4-0.gcp.cloud.qdrant.io:6333",
+    url="Your QDRANT_UI",
     api_key=os.getenv("QDRANT_API_KEY"),
     check_compatibility=False
 )
+
 
 print(client.get_collections())
